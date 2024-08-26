6299475ce1d185a94cf1b0d158cee979df4bb57e
mtarnowski <michal.tarnowski03@gmail.com>
2024-08-26 16:54:45 +0200

    gpt code test

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ fetch('wiersze.json')
     .then(wiersze => {
         // Dla każdego elementu w JSON generuj blok HTML
         for (const [name, fileName] of Object.entries(wiersze)) {
+            const figure = document.createElement('figure');
             figure.classList.add('custom-figure');
 
             const figcaption = document.createElement('figcaption');
